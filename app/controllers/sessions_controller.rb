3919b6cb516a17d7060bada501d4bac55b62dd3c
Spencer Carter <scmountain@gmail.com>
2017-05-22 11:03:59 -0600

    adds ability for login/sign up
    taking a break for lunch

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:session][:username])
-    
+
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       redirect_to root_path
@@ -18,6 +18,6 @@ class SessionsController < ApplicationController
   def destroy
       session.clear
       flash[:notice] = "You have successfully logged out"
-      redirect_to login_path
+      redirect_to signup_path
   end
 end
