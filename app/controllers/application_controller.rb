93807e61aee0c7c8333769eb328a942197585148
Spencer Carter <scmountain@gmail.com>
2017-05-22 11:03:59 -0600

    adds ability for login/sign up
    taking a break for lunch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,3 +10,6 @@ class ApplicationController < ActionController::Base
     redirect_to signup_path if !current_user
   end
 end
+def current_user
+  @current_user ||= User.find(session[:user_id]) if session[:user_id]
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root to: "links#index"
   get '/signup', to: 'users#new', as: "signup"
   post '/users', to: 'users#create'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   resources :links, only: [:index]
 
   namespace :api do
