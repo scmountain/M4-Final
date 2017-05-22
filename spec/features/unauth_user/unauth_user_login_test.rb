17a9856670bc357617f4da4652b63c45b17021e5
Spencer Carter <scmountain@gmail.com>
2017-05-22 09:44:33 -0600

    Fixes the login redirect
    Removes the permissions folder that was way too complicated for this task
    I was over complicating everything, returns app to simple user/nonuser

--- spec/features/unauth_user/unauth_user_login_test.rb
+++ spec/features/unauth_user/unauth_user_login_test.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "As unauthenticated user" do
   scenario "I am redirected to login" do
-    visit "/"
+    visit root_path
     expect(current_path).to eq(login_path)
   end
 end
