3c1a0c364670d9b752ec6b615188cf1f981d35d7
Spencer Carter <scmountain@gmail.com>
2017-05-22 15:39:26 -0600

    stuck on nilclass creation

--- spec/features/user/user-signs-up_test.rb
+++ spec/features/user/user-signs-up_test.rb
@@ -1,8 +1,12 @@
 require "rails_helper"
 
 RSpec.describe "As unauthenticated user" do
-  scenario "I am redirected to login" do
+  scenario "I am redirected to signup" do
     visit signup_path
-    # fill_in :
+
+    fill_in "Email", with: "BillytheGoat@example.com"
+    fill_in "Name", with: "janedoe"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password" :
   end
 end
