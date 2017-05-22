3a8150ffd37351cfad6d53995675590e9a9267a6
Spencer Carter <scmountain@gmail.com>
2017-05-22 10:07:50 -0600

    adds forms to view for login/signup

--- spec/features/unauth_user/unauth_user_login_test.rb
+++ spec/features/unauth_user/unauth_user_login_test.rb
@@ -4,5 +4,7 @@ RSpec.describe "As unauthenticated user" do
   scenario "I am redirected to login" do
     visit root_path
     expect(current_path).to eq(login_path)
+    expect(page).to have_content("Log In")
+    expect(page).to have_content("Sign Up")
   end
 end
