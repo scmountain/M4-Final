cf5e474f7fe1df306ded77b9a4394163395052da
Spencer Carter <scmountain@gmail.com>
2017-05-22 16:43:33 -0600

    allows the creation of a user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(name: params[:session][:username])
+    @user = User.find_by(login[:email])
 
-    if @user && @user.authenticate(params[:session][:password])
+    if @user && @user.authenticate(login[:password])
       session[:user_id] = @user.id
       redirect_to root_path
     else
@@ -19,5 +19,10 @@ class SessionsController < ApplicationController
       session.clear
       flash[:notice] = "You have successfully logged out"
       redirect_to signup_path
+  end
+
+private
+  def login
+    params.require(:session).permit(:email, :password)
   end
 end
