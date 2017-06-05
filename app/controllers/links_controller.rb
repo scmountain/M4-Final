e75b9b38eb677139745791901ae79ac095374d7f
Spencer Carter <scmountain@gmail.com>
2017-06-05 09:34:54 -0600

    adds current_user fixes in controller and edit
    adds to the links controller edit functionality along with the view,
    also fixes the error messages to be dynamic

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -6,8 +6,41 @@ class LinksController < ApplicationController
   end
 
   def create
-    user = current_user
-    link = user.links.new(link_params)
+    link = current_user.links.new(link_paclass LinksController < ApplicationController
+    before_action :check_registered_user
+    skip_before_filter :verify_authenticity_token, :only => [:create]
+  def index
+    @links = current_user.links
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    @link.update_attributes(link_params)
+    if @link.save
+      flash[:notice] = "Link #{@link.title} updated!"
+      redirect_to root_path
+    else
+      flash[:error] = format_errors(@link.errors.full_messages)
+      render :edit
+    end
+end
+
+
+private
+
+  def format_errors(errors)
+    errors.reduce(:+)
+  end
+
+  def link_params
+    params.require(:link).permit(:title, :url, :user_id)
+  end
+end
+rams)
     if link.save
       flash[:notice] = "Link #{link.title} created!"
       redirect_to root_path
